969b0e3831167cb729201d63f6eecac0ea9946b7
Amin Hasanzadeh <105168063+aminhsnzadeh@users.noreply.github.com>
2022-06-02 11:20:14 +0430

    Improved Todo List
    Now added editable content when you click on clipboard icon in lists. Also fixed some minor problems and bugs

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 let _head = document.getElementById('head')
 let _dec = document.getElementById('desc')
-let _lists = document.getElementById('lists')
 let _number = document.getElementsByClassName('listChild').length
-let _parent = document.getElementById('todo')
-let _info = document.getElementById('info')
+let _flag = ''
 function list() {
     if (_head.value == 0) {
         alert('Please Fill the inputs')
     } else if (_dec.value.length <= 50) {
-        _number++
-        _lists.innerHTML += '<div class="listChild"><article><h2>'+ _number + '. '+  _head.value +'</h2><p>- '+ _dec.value +'</p></article><span id="closing" class="bi-x"></span></div>'
-        _head.value = ''
-        _dec.value = ''
+        _number+=1
+        document.getElementById('lists').innerHTML += '<li class="listChild"><article><h2>'+ _number +'.'+  _head.value +'</h2><p>- '+ _dec.value +'</p></article><span onclick="_edit(this)" class="bi-clipboard"></span><span onclick="_close(this)" id="closing" class="bi-x"></span></li>'
+        _head.value = null
+        _dec.value = null
     } else {
         alert('Description must be 50 or lower characters.')
     }
-    let _close = document.querySelectorAll('#closing')
-    for (let i = 0; i<_close.length; i++) {
-        _close[i].onclick = function() {
-            this.parentNode.remove()
-        }
-    }
+}
+function _edit(self) {
+    _flag = self
+    let _tempf = self.previousElementSibling.firstChild.innerHTML
+    let _tempd = self.previousElementSibling.lastChild.innerHTML
+    _head.value = _tempf
+    _dec.value = _tempd
+    document.getElementById('change').style.display = 'block'
+    document.getElementById('sub').style.display = 'none'
+    document.getElementById('closeInfo').style.display = 'none'
+}
+function _change() {
+    _flag.previousElementSibling.firstChild.innerHTML = _head.value
+    _flag.previousElementSibling.lastChild.innerHTML = _dec.value
+    document.getElementById('change').style.display = 'none'
+    document.getElementById('sub').style.display = 'block'
+    document.getElementById('closeInfo').style.display = 'block'
+    _head.value = null
+    _dec.value = null
+}
+function _close(self) {
+    self.parentElement.remove()
 }
 function _transforms() {
-    _parent.classList.toggle('activepar')
-    _info.classList.toggle('activeinfo')
+    document.getElementById('todo').classList.toggle('activepar')
+    document.getElementById('info').classList.toggle('activeinfo')
 }
 function _closetrans() {
-    _parent.classList.remove('activepar')
-    _info.classList.remove('activeinfo')
+    document.getElementById('todo').classList.remove('activepar')
+    document.getElementById('info').classList.remove('activeinfo')
 }
